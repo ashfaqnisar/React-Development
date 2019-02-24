616217b049f7e83e0885409efcbb06eb622c064e
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-02-24 23:46:09 +0530

    Intial Commit -  Module 1 Video 2
    Signed-off-by: ashfaqnisar <ashfaqnisar00@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-    state = {counter: 0};
+class Button extends Component {
     handleClick = () => {
-        this.setState((previous) => {
-            return {
-                counter: previous.counter + 1
-            }
-        })
+        this.props.onClickFunction(this.props.incrementvalue)
     };
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <button onClick={this.handleClick}>
-                        {this.state.counter}
-                    </button>
-                </header>
-
+            <div className={"App"}>
+                <button onClick={this.handleClick}>
+                    +{this.props.incrementvalue}
+                </button>
             </div>
+
+        )
+    }
+}
+
+const Result = (props) => {
+    return (
+        <div className={"App"}>
+            <label>{props.counter}</label>
+        </div>
+    )
+};
+
+class App extends Component {
+    state = {counter: 0};
+
+    incrementCounter = (incrementvalue) => {
+        return (
+            this.setState((previous) => {
+                return {
+                    counter: previous.counter + incrementvalue
+
+                }
+            })
+        )
+    };
+    render() {
+        return (
+            <div>
+                <Button incrementvalue={1} onClickFunction={this.incrementCounter}/>
+                <Button incrementvalue={5} onClickFunction={this.incrementCounter}/>
+                <Button incrementvalue={10} onClickFunction={this.incrementCounter}/>
+                <Button incrementvalue={100} onClickFunction={this.incrementCounter}/>
+
+                <Result counter={this.state.counter}/>
+            </div>
+
         );
     }
 }
