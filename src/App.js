d2b30a2ca87337042928ef06fcd3de6470478d84
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-02-25 12:49:11 +0530

    Intial Commit -  Module 2 Video 1
    Signed-off-by: ashfaqnisar <ashfaqnisar00@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class Button extends Component {
-    handleClick = () => {
-        this.props.onClickFunction(this.props.incrementvalue)
-    };
-
-    render() {
-        return (
-            <div className={"App"}>
-                <button onClick={this.handleClick}>
-                    +{this.props.incrementvalue}
-                </button>
-            </div>
-
-        )
-    }
-}
-
-const Result = (props) => {
+const Card = (props) => {
     return (
         <div className={"App"}>
-            <label>{props.counter}</label>
+            <header className={"App-header"}>
+                <img className={"Image"} src={props.link}/>
+                <div className={"details"}>
+                    <div className={"Name"}>{props.name}</div>
+                    <div className={"Company"}>{props.company}</div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+let data = [
+    {
+        name: "Ashfaq",
+        link: "https://avatars0.githubusercontent.com/u/20638539?v=4",
+        company: "Ezerka"
+    },
+    {
+        name: "Vamshi",
+        link: "https://avatars3.githubusercontent.com/u/35305744?s=460&v=4",
+        company: "Ezerka"
+    },
+    {
+        name: "Vinay Reddy",
+        link: "https://i.ibb.co/0nszQbN/8e-YSs7c-L-400x400.jpg",
+        company: "Ezerka"
+    }
+];
+
+const CardList = (props) => {
+    return (
+        <div>
+            {props.cardData.map(mappedData => <Card {...mappedData}/>)}
         </div>
     )
 };
-
 class App extends Component {
-    state = {counter: 0};
 
-    incrementCounter = (incrementvalue) => {
-        return (
-            this.setState((previous) => {
-                return {
-                    counter: previous.counter + incrementvalue
-
-                }
-            })
-        )
-    };
     render() {
         return (
             <div>
-                <Button incrementvalue={1} onClickFunction={this.incrementCounter}/>
-                <Button incrementvalue={5} onClickFunction={this.incrementCounter}/>
-                <Button incrementvalue={10} onClickFunction={this.incrementCounter}/>
-                <Button incrementvalue={100} onClickFunction={this.incrementCounter}/>
-
-                <Result counter={this.state.counter}/>
+                <CardList cardData={data}/>
             </div>
 
         );
